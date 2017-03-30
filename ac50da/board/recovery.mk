e0013a35a0922301f81bccccf95b0d785cfc984d
SnowCatPDA <snowcat@vpro.ru>
2017-03-30 10:57:47 +0500

    FIX recovery

--- ac50da/board/recovery.mk
+++ ac50da/board/recovery.mk
@@ -1,8 +1,11 @@
-# Recovery
-#TARGET_RECOVERY_FSTAB := device/archos/ac50da/rootdir/etc/fstab.qcom
-
 #RECOVERY_VARIANT := twrp
-TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_FSTAB := device/archos/ac50da/rootdir/etc/twrp.fstab
+
+# Recovery
+ifneq ($(RECOVERY_VARIANT),true)
+	TARGET_RECOVERY_FSTAB := device/archos/ac50da/rootdir/etc/fstab.qcom
+else
+	TW_THEME := portrait_hdpi
+	RECOVERY_SDCARD_ON_DATA := true
+	TARGET_RECOVERY_FSTAB := device/archos/ac50da/rootdir/etc/twrp.fstab
+endif
 
