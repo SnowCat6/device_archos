241cc550c3503eb9ac560650c5329b32abbf62df
SnowCatPDA <snowcat@vpro.ru>
2017-03-31 02:04:05 +0500

    FIX sensors

---
+++ ac50da/sensors/bst_fifo/Android.mk
@@ -0,0 +1,3 @@
+ifneq ($(filter lineage_paella,$(TARGET_PRODUCT)),)
+include $(all-subdir-makefiles)
+endif
--- ac50da/sensors/calibration/Android.mk
+++ ac50da/sensors/calibration/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The CyanogenMod project
+# Copyright (C) 2016 The CyanogenMod project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,17 +14,5 @@
 
 
 LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES := accelcal.c
-
-LOCAL_PRELINK_MODULE := false
-
-LOCAL_MODULE := accelcal
-
-include $(BUILD_EXECUTABLE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
